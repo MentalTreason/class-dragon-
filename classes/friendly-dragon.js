0fbaa4cc89e8281534a845956dca047cd679e597
Jamal <jamal@DESKTOP-36B518Q>
2023-12-07 02:51:10 -0600

    EOD

--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,25 @@
-// Your code here 
+// Your code here
+
+const Dragon = require("./dragon");
+
+class FriendlyDragon extends Dragon {
+    constructor(name,color,lifeGoals,friend){
+        super(name,color)
+        this.lifeGoals = lifeGoals
+        this.friend = friend
+
+
+    }
+
+    hasLifeGoals() {
+
+    }
+
+
+    helpsPeople() {
+
+    }
+}
+
+
+module.exports = FriendlyDragon;
