e87dc7fc07c704bf7ebfe31ae5f173a6bd13ec65
Jeremy Gallen <jmgallen124@gmail.com>
2023-12-09 11:54:00 -0600

    Finished Dragons Project

--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -12,14 +12,29 @@ class FriendlyDragon extends Dragon {
     }
 
     hasLifeGoals() {
-
+        this.lifeGoals.forEach((goal) => console.log(`${this.name} likes to ${goal}`));
     }
 
 
     helpsPeople() {
-
+        return `${this.name} helps their friend ${this.friend}`;
     }
 }
+
+const puff = new FriendlyDragon(
+    "Puff",
+    "green",
+    [
+      "live by the sea",
+      "frolick in the autumn mist",
+      "help small children"
+    ],
+    "Jackie Paper"
+  );
+  console.log(puff);
+  console.log(puff.breathesFire()); //Friendly dragons can still breathe fire!
+  puff.hasLifeGoals();
+  console.log(puff.helpsPeople());
 
 
 module.exports = FriendlyDragon;
